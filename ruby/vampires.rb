ee507fe5c98cb45efc65e820cf70bd05f0d3f801
Dan McKeon <dpmckeon@gmail.com>
2017-01-25 13:26:47 -0800

    Add allergy question to vampires.rb

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -9,26 +9,19 @@ for i in 0..(employees_to_process - 1)
 	p "What is your name?"
 	user_name = gets.chomp
 
+	if user_name == "Drake Cula" || user_name == "Tu Fang"
+		vampire_name_match = true
+	else
+		vampire_name_match = false
+	end
+
 	p "How old are you?"
 	user_age = gets.chomp.to_i
 
 	p "What year were you born?"
 	user_birthyear = gets.chomp.to_i
 
-	p "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
-	user_garlicresponse = gets.chomp
-
-	p "Would you like to enroll in the company's health insurance? (Y/N)"
-	user_insureresponse = gets.chomp
-
-
 	user_agebybirthyear = CURRENT_YEAR - user_birthyear 
-
-	if user_name == "Drake Cula" || user_name == "Tu Fang"
-		vampire_name_match = true
-	else
-		vampire_name_match = false
-	end
 
 	if user_agebybirthyear == user_age
 		age_verified = true
@@ -36,11 +29,17 @@ for i in 0..(employees_to_process - 1)
 		age_verified = false
 	end
 
+	p "Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)"
+	user_garlicresponse = gets.chomp
+
 	if user_garlicresponse == 'Y'
 		user_garlicpref = true
 	else
 		user_garlicpref = false
 	end
+
+	p "Would you like to enroll in the company's health insurance? (Y/N)"
+	user_insureresponse = gets.chomp
 
 	if user_insureresponse == 'Y'
 		user_insurepref = true
@@ -48,11 +47,24 @@ for i in 0..(employees_to_process - 1)
 		user_insurepref = false
 	end
 
+	p "Name all of your allergies, one at a time. When finished, type done"
+
+	allergy = "tbd"
+		
+	until allergy == "done" || allergy =="sunshine"
+		allergy = gets.chomp
+	end
+
+	if allergy == "sunshine"
+		sunshine_allergy = true;
+	else 
+		sunshine_allergy = false;
+	end
 
 
-	if age_verified && (user_garlicpref || user_insurepref) && !vampire_name_match
+	if age_verified && (user_garlicpref || user_insurepref) && !vampire_name_match && !sunshine_allergy
 		vampire_result = "Probably not a vampire."
-	elsif !age_verified && (user_garlicpref || user_insurepref) && !vampire_name_match
+	elsif (!age_verified && (user_garlicpref || user_insurepref) && !vampire_name_match) || sunshine_allergy
 		vampire_result = "Probably a vampire."
 	elsif !(age_verified || user_garlicpref || user_insurepref) && !vampire_name_match
 		vampire_result = "Almost certainly a vampire."
@@ -63,8 +75,4 @@ for i in 0..(employees_to_process - 1)
 	end
 
 	p vampire_result
-	p "\n"
 end
-
-
-
