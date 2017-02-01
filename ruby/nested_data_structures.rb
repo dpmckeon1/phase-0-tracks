ac22f84b43517e23900cf1bcbb25119c63d7bf7c
Dan McKeon <dpmckeon@gmail.com>
2017-02-01 14:49:16 -0800

    Declare nested data structure for city_facts

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,43 @@
+city_facts = {
+	seattle: {
+		population: 652405, 
+		top_attractions: [
+			"Space Needle",
+			"Pike Place Market"
+		],
+		pro_teams: {
+			football: "Seahawks",
+			baseball: "Mariners",
+			soccer: "Sounders"
+		}
+	},
+	minneapolis: {
+		population: 400070,
+		top_attractions: [
+			"Mall of America",
+			"Cherry in the Spoon"
+		],
+		pro_teams: {
+			football: "Vikings",
+			baseball: "Twins",
+			basketball: "Timeberwolves"
+		}
+	},
+	chicago: {
+		population: 2720546,
+		top_attractions: [
+			"Navy Pier",
+			"Millennium Park"
+		],
+		pro_teams: {
+			football: "Bears",
+			baseball: [
+				"Cubs",
+				"White Sox"
+			],
+			basketball: "Bulls",
+			hockey: "Black Hawks",
+			soccer: "Fire"
+		}
+	}
+}
