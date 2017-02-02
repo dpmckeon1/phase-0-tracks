c78ee9c6f4099d65a6c80fa4216df11c1d8d6401
Dan McKeon <dpmckeon@gmail.com>
2017-02-02 14:42:13 -0800

    Code Release 0 search_array

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -6,3 +6,19 @@
 # 4) Search each item in array and find index where search integer is equal to array element
 # 5) Return index where searchh integer == array element or nil if not present
 
+def search_array(array, search_integer)
+	index = 0
+	while index < array.length
+		if array[index] == search_integer
+			return index
+		end
+		index += 1
+	end
+end
+
+arr = [42, 89, 23, 1]
+
+
+p search_array(arr, 24)
+
+
