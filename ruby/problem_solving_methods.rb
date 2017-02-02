faa35899143bd493ba9b7fdf8c94672b3a31b7a5
Dan McKeon <dpmckeon@gmail.com>
2017-02-02 14:16:54 -0800

    Pseudocode find integer in array

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,8 @@
+# Release 0 Pseudocode
+
+# 1) Create array of integers
+# 2) Declare search integer
+# 3) Pass array and search integer into method
+# 4) Search each item in array and find index where search integer is equal to array element
+# 5) Return index where searchh integer == array element or nil if not present
+
