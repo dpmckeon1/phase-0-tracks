5bce22949ee40684669ab964540eca051280bf50
Dan McKeon <dpmckeon@gmail.com>
2017-02-07 14:28:21 -0800

    Add code from Nick in pairing session

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,5 @@
+# PAIRING 6.2 WITH DAN McKEON
+
 class Puppy
 
   def fetch(toy)
@@ -5,5 +7,38 @@ class Puppy
     toy
   end
 
+  def speak(num_times)
+  	num_times.times {puts "Woof!"}
+  end
+
+  def roll_over
+  	puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+  	dog_age = human_years * 7
+  	dog_age
+  end
+
+  def sit(dog_name)
+  	puts "#{dog_name} is sitting like a dog" 
+  end
+
+  def initialize
+  	puts "Initializing new puppy instance..."
+  end
+
+
 end
+
+goober = Puppy.new
+goober.fetch("ball")
+
+goober.speak(5)
+
+goober.roll_over
+
+p goober.dog_years(4)
+
+goober.sit("Goober")
 
