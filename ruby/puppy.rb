150cc85c19ff25e53bcbbb4214610855132b6aa9
Dan McKeon <dpmckeon@gmail.com>
2017-02-06 19:36:58 -0800

    Add characteristics and behavior pseudocode to puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# SPECIES ------------------------
+# Dogus Puppyus
+
+# CHARACTERISTICS ----------------
+# Name: varies
+# Fur color: varies
+# Collar color: varies
+# Leg count: 4
+# Eye count: 2
+# Cute: true
+# Size: small
+
+# BEHAVIOR -----------------------
+# Wag tail
+# Jump
+# Sit
