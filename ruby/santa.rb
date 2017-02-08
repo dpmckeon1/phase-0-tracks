7cf3f1052fcdfb0c3824dc290db7e150010edfac
Dan McKeon <dpmckeon@gmail.com>
2017-02-08 15:07:11 -0800

    Create array of Santas in driver code and loop through

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -16,6 +16,19 @@ class Santa
 	end
 end
 
-santa_jim = Santa.new
+santa_jim = Santa.new("Male", "Hawaiian")
 santa_jim.speak
 santa_jim.eat_milk_and_cookies("Chocolate Chip Cookie")
+
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+santas.each { |santa|
+	puts santa.speak
+}
