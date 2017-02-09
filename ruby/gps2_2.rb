4364ba7d044cc2dd236868f9d3165a1bd71c5cf9
Dan McKeon <dpmckeon@gmail.com>
2017-02-08 21:18:11 -0800

    Add .rb at end of gps2_2

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,38 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# output: grocery list hash
+# steps: 
+  # 1) Store argument (string of items) separated into array by dividing it at each space
+  # 2) Populate a hash with that that string as the key and default value of 1
+  # 3) Print hash to console
+  # 4) Return hash
+
+def create_list(grocery_items)
+	
+
+end
+
+
+
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps:
+# output:
+
+# Method to remove an item from the list
+# input:
+# steps:
+# output:
+
+# Method to update the quantity of an item
+# input:
+# steps:
+# output:
+
+# Method to print a list and make it look pretty
+# input:
+# steps:
+# output:
+
+
