03be66dcc312c76fe68e90dfd5295796ec995509
Dan McKeon <dpmckeon@gmail.com>
2017-02-10 07:41:24 -0800

    Refactor shout.rb to include module as mixin to 2 new classes: Toddley and Baby; add driver code to test

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,16 +1,43 @@
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+
+# 	def self.yelling_happily(words)
+# 		"This is how I yell happily: " + words + "!!!"
+# 	end
+# end
+
+# angry_yelling = Shout.yell_angrily("Grrrrrrrrr")
+
+# happy_yelling = Shout.yelling_happily("Yayayayaya")
+
+# puts "Angry yelling: #{angry_yelling}"
+# puts "Happy yelling: #{happy_yelling}"
+
+# Mixin version
+
 module Shout
-	def self.yell_angrily(words)
+	def yell_angrily(words)
 		words + "!!!" + " :("
 	end
 
-	def self.yelling_happily(words)
+	def yelling_happily(words)
 		"This is how I yell happily: " + words + "!!!"
 	end
 end
 
-angry_yelling = Shout.yell_angrily("Grrrrrrrrr")
+class Toddler
+	include Shout
+end
 
-happy_yelling = Shout.yelling_happily("Yayayayaya")
+class Baby
+	include Shout
+end
 
-puts "Angry yelling: #{angry_yelling}"
-puts "Happy yelling: #{happy_yelling}"
+
+frankie = Toddler.new
+puts frankie.yell_angrily("Waaaaaaaaaa")
+
+suzie = Baby.new
+puts suzie.yelling_happily("Hehehehe")
