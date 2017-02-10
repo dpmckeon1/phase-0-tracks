bfd33008c1a07123b83cff93d3f168383a8b6474
Dan McKeon <dpmckeon@gmail.com>
2017-02-09 20:30:45 -0800

    Create 100 santas with diverse attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,6 @@
 class Santa
-	attr_reader :age, :ethnicity
-	attr_accessor :gender
+	attr_reader :ethnicity
+	attr_accessor :gender, :reindeer_ranking, :age
 
 	def initialize(gender, ethnicity)
 		@gender = gender
@@ -25,11 +25,9 @@ class Santa
 	end
 
 	def get_mad_at(reindeer_name)
-		puts "Before delete_at: #{@reindeer_ranking}"
 		reindeer_index = @reindeer_ranking.index(reindeer_name)
 		@reindeer_ranking.delete_at(reindeer_index)
 		@reindeer_ranking << reindeer_name
-		puts "After delete_at: #{@reindeer_ranking}"
 	end
 end
 
@@ -37,10 +35,13 @@ end
 # santa_jim.speak
 # santa_jim.eat_milk_and_cookies("Chocolate Chip Cookie")
 
-# genders = ["Male", "Female", "Cis", "Trans"]
-# ethnicities = ["White", "Latino", "Brazilian", "Portuguese", "Chinese", "Filipino"]
+# ages = [24, 48, 53, 25, 18, 39, 67]
+genders = ["Male", "Female", "Cis", "Trans"]
+ethnicities = ["White", "Latino", "Brazilian", "Portuguese", "Chinese", "Filipino"]
+reindeer = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+cookies = ["Chocolate Chip Cookie", "Snickerdoodle", "Thin Mint"]
 
-# diverse_random_santas = []
+diverse_random_santas = []
 
 # 10.times { |i|
 # 	# diverse_random_santas << Santa.new(genders.sample, ethnicities.sample)
@@ -61,10 +62,29 @@ end
 # 	homogenous_santas << Santa.new(genders[1], ethnicities[1])
 # }
 
-santa_jim = Santa.new("Male", "Hawaiian")
-santa_jim.celebrate_birthday
-santa_jim.get_mad_at("Vixen")
-santa_jim.gender = "Cis"
-puts santa_jim.gender
-puts santa_jim.age
-puts santa_jim.ethnicity
+# santa_jim = Santa.new("Male", "Hawaiian")
+# santa_jim.celebrate_birthday
+# santa_jim.get_mad_at("Vixen")
+# santa_jim.gender = "Cis"
+# puts santa_jim.gender
+# puts santa_jim.age
+# puts santa_jim.ethnicity
+
+
+100.times { |i|
+	diverse_random_santas[i] = Santa.new(genders.sample, ethnicities.sample)
+	diverse_random_santas[i].age = rand(140)
+	if i % 2 == 0
+		diverse_random_santas[i].celebrate_birthday
+	else
+		diverse_random_santas[i].get_mad_at(reindeer.sample)
+		diverse_random_santas[i].eat_milk_and_cookies(cookies.sample)
+	end
+}
+
+diverse_random_santas.each do |santa| 
+	puts "Santa id #{santa}...Age: #{santa.age}, Ethnicity: #{santa.ethnicity}, Gender: #{santa.gender}, 
+	Reindeer Ranking: #{santa.reindeer_ranking}" 
+end
+
+
