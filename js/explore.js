81fbb5811f5882ceb4a3534378596001123f2d26
Dan McKeon <dpmckeon@gmail.com>
2017-02-14 17:15:29 -0800

    pseudocode reverse function

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// reverse function pseudocode
+
+// input string to reverse
+// separate each character of string into array
+// put last character in array into new reversed array
+// put second to last character into second position and so on until 0th index
+// combine reversed array to create reversed string
+// return reversed string
+
