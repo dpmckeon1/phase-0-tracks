aab5c522dd087984f4c687b1d0f3174751d40fde
Dan McKeon <dpmckeon@gmail.com>
2017-02-16 17:36:36 -0800

    Initalize FindLongestPhrase function

---
+++ js/algos.js
@@ -0,0 +1,16 @@
+// find_longest_phrase function
+// input: array of strings
+// output: string
+
+// 1) split first string in array into array characters
+// 2) find length of said array
+// 3) store as longest_phrase
+// 4) test rest of strings in array and if any are longer, store in longest_phrase
+// 5) return longest_phrase
+
+function FindLongestPhrase(phrasesArray) {
+	var longestPhrase = "";
+	
+	var testPhrase = phrases
+
+}
