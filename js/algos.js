4567176bdf8691e2820212a85f82c489703de58e
Dan McKeon <dpmckeon@gmail.com>
2017-02-17 09:20:43 -0800

    Pseudocode has_matching_pair function and declare test variables

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,7 @@
+// Release 0
+
+console.log("*** Release 0 ***\n");
+
 // find_longest_phrase function
 // input: array of strings
 // output: string
@@ -30,3 +34,26 @@ console.log(FindLongestPhrase(phraseArray1));
 console.log(FindLongestPhrase(phraseArray2));
 
 console.log(FindLongestPhrase(phraseArray3));
+
+// Release 1
+
+console.log("\n*** Release 1 ***\n");
+
+// has_matching_pair function
+// input: 2 objects with key-value pairs
+// output: boolean
+
+// 1) Set has_matching_pair variable to false
+// 2) Loop through keys pairs in first object
+// 3) For each key in first object, use for loop to find 
+// matching key in second object
+// 4) If there is a matching key, see if the values match. If so, 
+// set has_matching_pair to true
+// 5) Return has_matching_pair
+
+
+
+
+
+var steven =  {name: "Steven", age: 54};
+var tamir = {name: "Tamir", age: 54};
